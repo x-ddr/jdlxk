178798c0d9b8e157b6ce641403a31e527235e9ee
GitHub Action <action@github.com>
2021-04-12 19:38:05 +0800

    ⌚️20210412193805

--- old/lxk20210412193638/jd_shop.js
+++ old/lxk20210412193638/jd_shop.js
@@ -1,27 +1,24 @@
 /*
-脚本：取关京东店铺和商品
-更新时间：2021-04-12
-因种豆得豆和宠汪汪以及NobyDa大佬的京东签到脚本会关注店铺和商品，故此脚本用来取消已关注的店铺和商品
-默认每运行一次脚本取消关注10个商品，10个店铺。可结合boxjs自定义取消多少个（目前测试通过最大数量是一次性取消300个商品无异常，大于300请自行测试，建议尽量不要一次性全部取消以免出现问题）。
-建议此脚本运行时间在 种豆得豆和宠汪汪脚本运行之后 再执行
-现有功能: 1、取关商品。2、取关店铺。3、匹配到boxjs输入的过滤关键词后，不再进行此商品/店铺后面(包含输入的关键词商品/店铺)的取关。4、支持京东双账号
-脚本兼容: Quantumult X, Surge, Loon, JSBox, Node.js, 小火箭
-==============Quantumult X===========
+进店领豆,每天可拿四京豆
+活动入口：京东APP首页-领京豆-进店领豆
+更新时间：2020-11-03
+已支持IOS双京东账号,Node.js支持N个京东账号
+脚本兼容: QuantumultX, Surge, Loon, JSBox, Node.js
+===============Quantumultx===============
 [task_local]
-#取关京东店铺商品
-55 23 * * * https://gitee.com/lxk0301/jd_scripts/raw/master/jd_unsubscribe.js, tag=取关京东店铺商品, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
-===========Loon============
+#进店领豆
+10 0 * * * https://gitee.com/lxk0301/jd_scripts/raw/master/jd_shop.js, tag=进店领豆, img-url=https://raw.githubusercontent.com/58xinian/icon/master/jd_shop.png, enabled=true
+================Loon============
 [Script]
-cron "55 23 * * *" script-path=https://gitee.com/lxk0301/jd_scripts/raw/master/jd_unsubscribe.js,tag=取关京东店铺商品
-============Surge=============
-取关京东店铺商品 = type=cron,cronexp="55 23 * * *",wake-system=1,timeout=3600,script-path=https://gitee.com/lxk0301/jd_scripts/raw/master/jd_unsubscribe.js
-===========小火箭========
-取关京东店铺商品 = type=cron,script-path=https://gitee.com/lxk0301/jd_scripts/raw/master/jd_unsubscribe.js, cronexpr="55 23 * * *", timeout=3600, enable=true
- */
-const $ = new Env('取关京东店铺和商品');
+cron "10 0 * * *" script-path=https://gitee.com/lxk0301/jd_scripts/raw/master/jd_shop.js,tag=进店领豆
+==============Surge===============
+[Script]
+进店领豆 = type=cron,cronexp="10 0 * * *",wake-system=1,timeout=3600,script-path=https://gitee.com/lxk0301/jd_scripts/raw/master/jd_shop.js
+*/
+const $ = new Env('进店领豆');
+const notify = $.isNode() ? require('./sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
-const notify = $.isNode() ? require('./sendNotify') : '';
 
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [], cookie = '';
@@ -33,15 +30,13 @@ if ($.isNode()) {
 } else {
   cookiesArr = [$.getdata('CookieJD'), $.getdata('CookieJD2'), ...jsonParse($.getdata('CookiesJD') || "[]").map(item => item.cookie)].filter(item => !!item);
 }
-const jdNotify = $.getdata('jdUnsubscribeNotify');//是否关闭通知，false打开通知推送，true关闭通知推送
-let goodPageSize = $.getdata('jdUnsubscribePageSize') || 20;// 运行一次取消多少个已关注的商品。数字0表示不取关任何商品
-let shopPageSize = $.getdata('jdUnsubscribeShopPageSize') || 20;// 运行一次取消多少个已关注的店铺。数字0表示不取关任何店铺
-let stopGoods = $.getdata('jdUnsubscribeStopGoods') || '';//遇到此商品不再进行取关，此处内容需去商品详情页（自营处）长按拷贝商品信息
-let stopShop = $.getdata('jdUnsubscribeStopShop') || '';//遇到此店铺不再进行取关，此处内容请尽量从头开始输入店铺名称
-const JD_API_HOST = 'https://wq.jd.com/fav';
+let message = '', subTitle = '';
+
+const JD_API_HOST = 'https://api.m.jd.com/client.action';
 !(async () => {
   if (!cookiesArr[0]) {
-    $.msg('【京东账号一】取关京东店铺商品失败', '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', {"open-url": "https://bean.m.jd.com/bean/signIndex.action"});
+    $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', {"open-url": "https://bean.m.jd.com/bean/signIndex.action"});
+    return;
   }
   for (let i = 0; i < cookiesArr.length; i++) {
     if (cookiesArr[i]) {
@@ -60,9 +55,9 @@ const JD_API_HOST = 'https://wq.jd.com/fav';
         }
         continue
       }
-      await requireConfig();
-      await jdUnsubscribe();
-      await showMsg();
+      message = '';
+      subTitle = '';
+      await jdShop();
     }
   }
 })()
@@ -72,203 +67,98 @@ const JD_API_HOST = 'https://wq.jd.com/fav';
     .finally(() => {
       $.done();
     })
-async function jdUnsubscribe(doubleKey) {
-  await Promise.all([
-    unsubscribeGoods(doubleKey),
-    unsubscribeShops()
-  ])
-  await Promise.all([
-    getFollowShops(),
-    getFollowGoods()
-  ])
-}
-function showMsg() {
-  if (!jdNotify || jdNotify === 'false') {
-    $.msg($.name, ``, `【京东账号${$.index}】${$.nickName}\n【已取消关注店铺】${$.unsubscribeShopsCount}个\n【已取消关注商品】${$.unsubscribeGoodsCount}个\n【还剩关注店铺】${$.shopsTotalNum}个\n【还剩关注商品】${$.goodsTotalNum}个\n`);
-  } else {
-    $.log(`\n【京东账号${$.index}】${$.nickName}\n【已取消关注店铺】${$.unsubscribeShopsCount}个\n【已取消关注商品】${$.unsubscribeGoodsCount}个\n【还剩关注店铺】${$.shopsTotalNum}个\n【还剩关注商品】${$.goodsTotalNum}个\n`);
+async function jdShop() {
+  const taskData = await getTask();
+  if (taskData.code === '0') {
+    if (!taskData.data.taskList) {
+      console.log(`${taskData.data.taskErrorTips}\n`);
+      $.msg($.name, '', `京东账号 ${$.index} ${$.nickName}\n${taskData.data.taskErrorTips}`);
+    } else {
+      const { taskList } = taskData.data;
+      let beanCount = 0;
+      for (let item of taskList) {
+        if (item.taskStatus === 3) {
+          console.log(`${item.shopName} 已拿到2京豆\n`)
+        } else {
+          console.log(`taskId::${item.taskId}`)
+          const doTaskRes = await doTask(item.taskId);
+          if (doTaskRes.code === '0') {
+            beanCount += 2;
+          }
+        }
+      }
+      console.log(`beanCount::${beanCount}`);
+      if (beanCount > 0) {
+        $.msg($.name, '', `京东账号 ${$.index} ${$.nickName}\n成功领取${beanCount}京豆`);
+        // if ($.isNode()) {
+        //   await notify.sendNotify(`${$.name} - 账号${$.index} - ${$.nickName}`, `京东账号${$.index} ${UserName}\n成功领取${beanCount}京豆`);
+        // }
+        // if ($.isNode()) {
+        //   await notify.BarkNotify(`${$.name}`, `京东账号${$.index} ${UserName}\n成功领取${beanCount}京豆`);
+        // }
+      }
+    }
   }
 }
-function unsubscribeGoods() {
-  return new Promise(async (resolve) => {
-    let followGoods = await getFollowGoods();
-    if (followGoods.iRet === '0') {
-      let count = 0;
-      $.unsubscribeGoodsCount = count;
-      if ((goodPageSize * 1) !== 0) {
-        if (followGoods.totalNum > 0) {
-          for (let item of followGoods.data) {
-
-            console.log(`是否匹配：：${item.commTitle.indexOf(stopGoods.replace(/\ufffc|\s*/g, ''))}`)
-
-            if (stopGoods && item.commTitle.indexOf(stopGoods.replace(/\ufffc|\s*/g, '')) > -1) {
-              console.log(`匹配到了您设定的商品--${stopGoods}，不在进行取消关注商品`)
-              break;
-            }
-            let res = await unsubscribeGoodsFun(item.commId);
-            // console.log('取消关注商品结果', res);
-            if (res.iRet === 0 && res.errMsg === 'success') {
-              console.log(`取消关注商品---${item.commTitle.substring(0, 20).concat('...')}---成功\n`)
-              count ++;
-            } else {
-              console.log(`取消关注商品---${item.commTitle.substring(0, 20).concat('...')}---失败\n`)
-            }
-          }
-          $.unsubscribeGoodsCount = count;
-          resolve(count)
-        } else {
-          resolve(count)
-        }
-      } else {
-        console.log(`\n您设置的是不取关商品\n`);
-        resolve(count);
-      }
-    }
-  })
-}
-function getFollowGoods() {
-  return new Promise((resolve) => {
-    const option = {
-      url: `${JD_API_HOST}/comm/FavCommQueryFilter?cp=1&pageSize=${goodPageSize}&_=${Date.now()}&category=0&promote=0&cutPrice=0&coupon=0&stock=0&areaNo=1_72_4139_0&sceneval=2&g_login_type=1&callback=jsonpCBKB&g_ty=ls`,
-      headers: {
-        "Host": "wq.jd.com",
-        "Accept": "*/*",
-        "Connection": "keep-alive",
-        "Cookie": cookie,
-        "User-Agent": $.isNode() ? (process.env.JD_USER_AGENT ? process.env.JD_USER_AGENT : (require('./USER_AGENTS').USER_AGENT)) : ($.getdata('JDUA') ? $.getdata('JDUA') : "jdapp;iPhone;9.4.4;14.3;network/4g;Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1"),
-        "Accept-Language": "zh-cn",
-        "Referer": "https://wqs.jd.com/my/fav/goods_fav.shtml?ptag=37146.4.1&sceneval=2&jxsid=15963530166144677970",
-        "Accept-Encoding": "gzip, deflate, br"
-      },
-    }
-    $.get(option, (err, resp, data) => {
-      try {
-        data = JSON.parse(data.slice(14, -13));
-        $.goodsTotalNum = data.totalNum;
-        // console.log('data', data.data.length)
-      } catch (e) {
-        $.logErr(e, resp);
-      } finally {
-        resolve(data);
-      }
-    });
-  })
-}
-function unsubscribeGoodsFun(commId) {
+function doTask(taskId) {
+  console.log(`doTask-taskId::${taskId}`)
   return new Promise(resolve => {
-    const option = {
-      url: `${JD_API_HOST}/comm/FavCommDel?commId=${commId}&_=${Date.now()}&sceneval=2&g_login_type=1&callback=jsonpCBKP&g_ty=ls`,
+    const body = { 'taskId': `${taskId}` };
+    const options = {
+      url: `${JD_API_HOST}`,
+      body: `functionId=takeTask&body=${escape(JSON.stringify(body))}&appid=ld`,
       headers: {
-        "Host": "wq.jd.com",
-        "Accept": "*/*",
-        "Connection": "keep-alive",
         'User-Agent': $.isNode() ? (process.env.JD_USER_AGENT ? process.env.JD_USER_AGENT : (require('./USER_AGENTS').USER_AGENT)) : ($.getdata('JDUA') ? $.getdata('JDUA') : "jdapp;iPhone;9.4.4;14.3;network/4g;Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1"),
-        'Referer': 'https://wqs.jd.com/my/fav/goods_fav.shtml?ptag=37146.4.1&sceneval=2&jxsid=15963530166144677970',
+        'Host': 'api.m.jd.com',
+        'Content-Type': 'application/x-www-form-urlencoded',
         'Cookie': cookie,
-        "Accept-Language": "zh-cn",
-        "Accept-Encoding": "gzip, deflate, br"
-      },
-    }
-    $.get(option, (err, resp, data) => {
-      try {
-        data = JSON.parse(data.slice(14, -13).replace(',}', '}'));
-        // console.log('data', data);
-        // console.log('data', data.errMsg);
-      } catch (e) {
-        $.logErr(e, resp);
-      } finally {
-        resolve(data);
       }
-    });
-  })
-}
-
-function unsubscribeShops() {
-  return new Promise(async (resolve) => {
-    let followShops = await getFollowShops();
-    if (followShops.iRet === '0') {
-      let count = 0;
-      $.unsubscribeShopsCount = count;
-      if ((shopPageSize * 1) !== 0) {
-        if (followShops.totalNum > 0) {
-          for (let item of followShops.data) {
-            if (stopShop && (item.shopName && item.shopName.indexOf(stopShop.replace(/\s*/g, '')) > -1)) {
-              console.log(`匹配到了您设定的店铺--${item.shopName}，不在进行取消关注店铺`)
-              break;
-            }
-            let res = await unsubscribeShopsFun(item.shopId);
-            // console.log('取消关注店铺结果', res);
-            if (res.iRet === '0') {
-              console.log(`取消已关注店铺---${item.shopName}----成功\n`)
-              count ++;
-            } else {
-              console.log(`取消已关注店铺---${item.shopName}----失败\n`)
-            }
-          }
-          $.unsubscribeShopsCount = count;
-          resolve(count)
+    }
+    $.post(options, (err, resp, data) => {
+      try {
+        if (err) {
+          console.log('\n进店领豆: API查询请求失败 ‼️‼️')
+          $.logErr(err);
         } else {
-          resolve(count)
+          // console.log(data)
+          data = JSON.parse(data);
         }
-      } else {
-        console.log(`\n您设置的是不取关店铺\n`);
-        resolve(count)
-      }
-    }
-  })
-}
-function getFollowShops() {
-  return new Promise((resolve) => {
-    const option = {
-      url: `${JD_API_HOST}/shop/QueryShopFavList?cp=1&pageSize=${shopPageSize}&_=${Date.now()}&sceneval=2&g_login_type=1&callback=jsonpCBKA&g_ty=ls`,
-      headers: {
-        "Host": "wq.jd.com",
-        "Accept": "*/*",
-        "Connection": "keep-alive",
-        "Cookie": cookie,
-        "User-Agent": $.isNode() ? (process.env.JD_USER_AGENT ? process.env.JD_USER_AGENT : (require('./USER_AGENTS').USER_AGENT)) : ($.getdata('JDUA') ? $.getdata('JDUA') : "jdapp;iPhone;9.4.4;14.3;network/4g;Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1"),
-        "Accept-Language": "zh-cn",
-        "Referer": "https://wqs.jd.com/my/fav/shop_fav.shtml?sceneval=2&jxsid=15963530166144677970&ptag=7155.1.9",
-        "Accept-Encoding": "gzip, deflate, br"
-      },
-    }
-    $.get(option, (err, resp, data) => {
-      try {
-        data = JSON.parse(data.slice(14, -13));
-        $.shopsTotalNum = data.totalNum;
       } catch (e) {
         $.logErr(e, resp);
       } finally {
         resolve(data);
       }
-    });
+    })
   })
 }
-function unsubscribeShopsFun(shopId) {
-  return new Promise(resolve => {
-    const option = {
-      url: `${JD_API_HOST}/shop/DelShopFav?shopId=${shopId}&_=${Date.now()}&sceneval=2&g_login_type=1&callback=jsonpCBKG&g_ty=ls`,
-      headers: {
-        "Host": "wq.jd.com",
-        "Accept": "*/*",
-        "Connection": "keep-alive",
-        'User-Agent': $.isNode() ? (process.env.JD_USER_AGENT ? process.env.JD_USER_AGENT : (require('./USER_AGENTS').USER_AGENT)) : ($.getdata('JDUA') ? $.getdata('JDUA') : "jdapp;iPhone;9.4.4;14.3;network/4g;Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1"),
-        'Referer': 'https://wqs.jd.com/my/fav/shop_fav.shtml?sceneval=2&jxsid=15960121319555534107&ptag=7155.1.9',
-        'Cookie': cookie,
-        "Accept-Language": "zh-cn",
-        "Accept-Encoding": "gzip, deflate, br"
-      },
-    }
-    $.get(option, (err, resp, data) => {
-      try {
-        data = JSON.parse(data.slice(14, -13));
-      } catch (e) {
-        $.logErr(e, resp);
-      } finally {
-        resolve(data);
-      }
-    });
-  })
+function getTask(body = {}) {
+ return new Promise(resolve => {
+   const options = {
+     url: `${JD_API_HOST}`,
+     body: `functionId=queryTaskIndex&body=${escape(JSON.stringify(body))}&appid=ld`,
+     headers: {
+       'User-Agent': $.isNode() ? (process.env.JD_USER_AGENT ? process.env.JD_USER_AGENT : (require('./USER_AGENTS').USER_AGENT)) : ($.getdata('JDUA') ? $.getdata('JDUA') : "jdapp;iPhone;9.4.4;14.3;network/4g;Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1"),
+       'Host': 'api.m.jd.com',
+       'Content-Type': 'application/x-www-form-urlencoded',
+       'Cookie': cookie,
+     }
+   }
+   $.post(options, (err, resp, data) => {
+     try {
+       if (err) {
+         console.log('\n进店领豆: API查询请求失败 ‼️‼️')
+         $.logErr(err);
+       } else {
+         // console.log(data)
+         data = JSON.parse(data);
+       }
+     } catch (e) {
+       $.logErr(e, resp);
+     } finally {
+       resolve(data);
+     }
+   })
+ })
 }
 function TotalBean() {
   return new Promise(async resolve => {
@@ -312,27 +202,6 @@ function TotalBean() {
         resolve();
       }
     })
-  })
-}
-function requireConfig() {
-  return new Promise(resolve => {
-    if ($.isNode() && process.env.UN_SUBSCRIBES) {
-      if (process.env.UN_SUBSCRIBES.indexOf('&') > -1) {
-        $.UN_SUBSCRIBES = process.env.UN_SUBSCRIBES.split('&');
-      } else if (process.env.UN_SUBSCRIBES.indexOf('\n') > -1) {
-        $.UN_SUBSCRIBES = process.env.UN_SUBSCRIBES.split('\n');
-      } else if (process.env.UN_SUBSCRIBES.indexOf('\\n') > -1) {
-        $.UN_SUBSCRIBES = process.env.UN_SUBSCRIBES.split('\\n');
-      } else {
-        $.UN_SUBSCRIBES = process.env.UN_SUBSCRIBES.split();
-      }
-      console.log(`您secret设置的取关参数:\n${JSON.stringify($.UN_SUBSCRIBES)}`)
-      goodPageSize = $.UN_SUBSCRIBES[0] || goodPageSize;
-      shopPageSize = $.UN_SUBSCRIBES[1] || shopPageSize;
-      stopGoods = $.UN_SUBSCRIBES[2] || stopGoods;
-      stopShop = $.UN_SUBSCRIBES[3] || stopShop;
-    }
-    resolve()
   })
 }
 function jsonParse(str) {
